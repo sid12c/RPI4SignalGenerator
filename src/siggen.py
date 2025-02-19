2dd2416cd7e965d63e078b59e727c0d5fcbb4599
Sidharth Chaudhary <sid12c@gmail.com>
2025-02-19 13:21:59 -0500

    Added version information

--- src/siggen.py
+++ src/siggen.py
@@ -1,28 +1,41 @@
+__version__ = '1.1'
+__author__ = 'Sid Chaudhary'
+
 import time
 import numpy as np
 import pyaudio
 import argparse
+# from _version import __version__
 
 # parse command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("type",
-                    choices=["sine", "square"],
-                    help="output type of the signal",
-                    default="sine",
-                    const="sine",
-                    nargs="?")
-parser.add_argument("-d", "--duration",
-                    help="duration of waveform output in seconds",
+parser.add_argument('-v', '--version',
+                    action='version',
+                    version='%(prog)s ' + __version__
+                    )
+parser.add_argument('type',
+                    choices=['sine', 'square'],
+                    help='output type of the signal',
+                    default='sine',
+                    const='sine',
+                    nargs='?'
+                    )
+parser.add_argument('-d', '--duration',
+                    help='duration of waveform output in seconds',
                     type=float,
-                    default=10.0)
-parser.add_argument("-f", "--frequency",
-                    help="wave frequency in Hz",
+                    default=10.0
+                    )
+parser.add_argument('-f', '--frequency',
+                    help='wave frequency in Hz',
                     type=float,
-                    default=440.0)
-parser.add_argument("-a", "--amplitude",
-                    help="wave amplitude from 0.0 to 1.0",
+                    default=440.0
+                    )
+parser.add_argument('-a', '--amplitude',
+                    help='wave amplitude from 0.0 to 1.0',
                     type=float,
-                    default=0.5)
+                    default=0.5
+                    )
+
 args = parser.parse_args()
 
 p = pyaudio.PyAudio()
@@ -33,18 +46,18 @@ duration = args.duration  # in seconds, may be float
 f = args.frequency  # sine frequency, Hz, may be float
 
 # generate samples, note conversion to float32 array
-if args.type == "sine":
+if args.type == 'sine':
     samples = (np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
-if args.type == "square":
+if args.type == 'square':
     samples = (np.sign(np.sin(2 * np.pi * np.arange(fs * duration) * f / fs))).astype(np.float32)
 
 # per @yahweh comment explicitly convert to bytes sequence
 output_bytes = (amplitude * samples).tobytes()
 
-if args.type == "sine":
-    print("Playing {:.1f} Hz sine wave".format(f))
-if args.type == "square":
-    print("Playing {:.1f} Hz square wave".format(f))
+if args.type == 'sine':
+    print('Playing {:.1f} Hz sine wave'.format(f))
+if args.type == 'square':
+    print('Playing {:.1f} Hz square wave'.format(f))
 
 # for paFloat32 sample values must be in range [-1.0, 1.0]
 stream = p.open(format=pyaudio.paFloat32,
@@ -55,7 +68,7 @@ stream = p.open(format=pyaudio.paFloat32,
 # play. May repeat with different amplitude values (if done interactively)
 start_time = time.time()
 stream.write(output_bytes)
-print("Waveform output for {:.2f} seconds".format(time.time() - start_time))
+print('Waveform output for {:.2f} seconds'.format(time.time() - start_time))
 
 stream.stop_stream()
 stream.close()
